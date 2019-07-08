975787ada388c1742c7776308ba1d08933714a01
DESKTOP-U235624\fractal <gamesthatcan@gmail.com>
2019-07-08 00:54:15 -0400

    moved on with adding walk animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState {
+    walk, 
+    attack,
+    interact
+}
+
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D myRigidBody;
+    private Animator animator;
     private Vector3 change; 
+    private PlayerState currentState;
 
     // Start is called before the first frame update
     void Start()
     {
+        currentState = PlayerState.walk;
+        animator = GetComponent<Animator>();
         myRigidBody = GetComponent<Rigidbody2D>();
-        
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
     }
 
     // Update is called once per frame
@@ -21,16 +32,36 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
+        if(Input.GetButtonDown("attack")) 
+        {
+
+        }
+        if (currentState == PlayerState.walk)
+        {
+            UpdateAnimationAndMove();
+        }
+    }
+
+    void UpdateAnimationAndMove() 
+    {
         if(change != Vector3.zero) 
         {
             MoveCharacter();
-        }    
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }   
+        else 
+        {
+            animator.SetBool("moving", false);
+        }
     }
 
     void MoveCharacter() 
     {
+        change.Normalize();
         myRigidBody.MovePosition(
-            transform.position + Vector3.Normalize(change) * speed * Time.deltaTime
+            transform.position + change * speed * Time.deltaTime
         );
     }
 }
